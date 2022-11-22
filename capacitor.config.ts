05c3860dcd8b3177d9a137e8ff03ec2504613c8d
HananeArrazouki <hanane.arrazouki1999@gmail.com>
2022-11-22 22:48:42 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HairStyle',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
