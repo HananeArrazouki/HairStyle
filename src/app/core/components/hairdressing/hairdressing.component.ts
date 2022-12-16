9f974347d62ba8977caccef0ad0de37f519153c8
HananeArrazouki <hanane.arrazouki1999@gmail.com>
2022-12-16 01:28:06 +0100

    mod

--- src/app/core/components/hairdressing/hairdressing.component.ts
+++ src/app/core/components/hairdressing/hairdressing.component.ts
@@ -8,12 +8,6 @@ import { Hairdressing } from '../../interfaces/hairdressing';
 })
 export class HairdressingComponent {
 
-  //private _hairdressing : Hairdressing = {id: 1, name: "", price: 0, image: ""}
-  
-
-  // @Input('hairdressing') set hairdressingOption(hairdressing: Hairdressing) {
-  //     this._hairdressing = hairdressing
-  // }
   @Output() onEdit = new EventEmitter;
   @Output() onDelete = new EventEmitter;
   @Input() hairdressing : Hairdressing = {id: 0, name: "", price: 0, image: ""}
@@ -27,17 +21,4 @@ export class HairdressingComponent {
     this.onDelete.emit(this.hairdressing);
   }
   
-  // constructor(
-  //   private _hairdressingService: HairdressingService,
-  //   ) { }
-
-    //Get the list of the options
-  // get hairdressingOption() {
-  //   return this._hairdressing
-  // }
-
-
-  
-
-
 }
